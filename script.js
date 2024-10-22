c477027c097ec510515149a8164527e324af2705
Oghenetega16 <sukuruoghenetega@gmail.com>
2024-10-22 10:35:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#hidebox').click(function(){
+    $('#box').hide();
+});
+
+$('#showbox').click(function(){
+    $('#box').show();
+});
+
+$('#togglebox').click(function(){
+    $('#box').toggle();
+});
